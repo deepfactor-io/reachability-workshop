f497493595c1c195c5e1e588b7326f67aa9d2102
Mike Larkin <mlarkin@deepfactor.io>
2024-03-28 20:24:29 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# Coming Soon...
+# Java Agent code for the Deepfactor OWASP Reachability Workshop
+
+This code is a sample javaassist-based agent that records which
+classes have been loaded (eg, used). At periodic intervals, it
+prints the list of classes as a JSON blob to stdout.
+
+## Building
+
+You will need gradle 6.1 or later installed. To build, run
+_gradle shawdowJar_ from the root of the repository.
---
+++ df-agent/src/main/java/com/deepfactor/agent/Agent.java
@@ -0,0 +1,167 @@
+/*
+ * Agent.java
+ *
+ * (c) 2021 DeepFactor, Inc.
+ * All Rights Reserved
+ */
+package com.deepfactor.agent;
+
+import java.io.*;
+import java.lang.instrument.*;
+import java.net.*;
+import java.util.*;
+import java.security.*;
+import javassist.*;
+
+
+public class Agent implements ClassFileTransformer
+{
+	/* max number of loaded classes to queue */
+	public static int CLASSES_LIST_MAX = 10000;
+
+	/* seconds between flush intervals */
+	public static int FLUSH_INTERVAL_SECONDS = 15;
+
+	/* Inner class to store list of loaded classes */
+	private class ClassData {
+		public String className;
+		public ClassLoader loader;
+		public Vector<String> allMethods;
+
+		public ClassData(String className, ClassLoader loader) {
+			this.className = new String(className);
+			this.loader=loader;
+			allMethods = new Vector<String>();
+		}
+
+		public String getJson() {
+			StringBuilder json = new StringBuilder();
+			String source = "unknown";
+			boolean first = true;
+			boolean checkSource = true;
+
+			json.append("{ ");
+			json.append("\"class\": \"").append(className).append( "\"}");
+
+			return json.toString();
+		}
+	};
+
+	private static Vector<ClassData> allClasses;
+	private static Thread flushThread;
+	private static boolean flushThreadWaiting = false;
+
+	/*
+	 * flushClasses
+	 *
+	 * Send list of loaded classes to dfeventd
+	 */
+	public static synchronized void flushClasses() {
+		StringBuilder json = new StringBuilder();
+		Vector<ClassData> removeList = new Vector<ClassData>();
+		boolean first = true;
+
+		json.append("{ ").append("\"classes_loaded\": [ ");
+
+		try {
+			for (ClassData cd : allClasses) {
+				if (!first) {
+					json.append(", ");
+				}
+
+				first = false;
+
+				json.append(cd.getJson());
+				/*
+				 * Cannot remove from an iterator while
+				 * iterating without generating a
+				 * ConcurrentModificationException, so queue
+				 * the list of things to remove and remove
+				 * at the end.
+				 *
+				 * If we don't get all the elements in this pass
+				 * (for example, if another thread adds an
+				 * element while we are iterating), we will just
+				 * pick it up on the next pass.
+				 */
+				removeList.add(cd);
+			}
+		} catch (ConcurrentModificationException e) {
+		}
+		json.append(" ] }");
+		System.out.println(json);
+
+		allClasses.removeAll(removeList);
+	}
+
+	/*
+	 * transform
+	 *
+	 * Instrumentation method called on class load.
+	 *
+	 * Parameters:
+	 *  See ClassFileTransformer
+	 *
+	 * Returns:
+	 *  Same bytecode
+	 */
+	public byte[] transform(ClassLoader loader, String className,
+	    Class classBeingRedefined, ProtectionDomain protectionDomain,
+	    byte[] classfileBuffer) throws IllegalClassFormatException
+	{
+		byte[] buf = classfileBuffer;
+		if (className == null) {
+			return buf;
+		}
+		String classNameDotted = className.replaceAll("/", ".");
+		String source;
+
+		allClasses.add(new ClassData(classNameDotted, loader));
+		return buf;
+	}
+
+	/*
+	 * premain
+	 *
+	 * Agent startup code. This initializes the class file transformer
+	 * as well as registering a shutdown hook to flush any pending events
+	 * on shutdown. This method also starts a thread that periodically
+	 * flushes class load events, based on a timer.
+	 */
+	public static void premain(String args, Instrumentation inst)
+	{
+		System.out.println("Running Runtime Usage Java Agent");
+
+		allClasses = new Vector<ClassData>();
+		inst.addTransformer(new Agent(), true);
+
+		Runtime.getRuntime().addShutdownHook(
+			new Thread() {
+				public void run() {
+					com.deepfactor.agent.Agent.flushClasses();
+				}
+			});
+
+		flushThread = new Thread() {
+			public void run() {
+				while (true) {
+					try {
+						synchronized(this) {
+							flushThreadWaiting = true;
+							wait(
+							    FLUSH_INTERVAL_SECONDS * 1000);
+							flushThreadWaiting = false;
+						}
+						com.deepfactor.agent.Agent.flushClasses();
+					/* catch any exception, elastic checks thread exit status */
+					}
+					catch (InterruptedException e) {}
+					catch (Throwable e) {}
+				}
+			}
+		};
+
+		flushThread.setDaemon(true);
+		flushThread.start();
+	}
+}
